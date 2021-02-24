cd9a42c81104f96dd746cb04e71c5351671435a6
SyedNoahSadiq <sns82304@creighton.edu>
2021-02-24 15:09:47 -0600

    Initial commit

---
+++ forms/Pets/Pets.js
@@ -0,0 +1,13 @@
+let petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+console.log(petNames)
+
+let withSal = petNames
+
+petNames.push("Sal")
+
+console.log(`The third pet is named ${petNames[2]}`)
+
+console.log(petNames.length)
+
+petNames.pop()
+console.log(petNames)
